15da5546254e3e8b79145dbc6d6fb01c13dc8419
yoyokazoo <peter.beckfield@gmail.com>
2020-10-11 14:03:20 -0700

    Initial Commit.  Open Google.

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace ContestEnterer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_open_browser = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn_open_browser
+            // 
+            this.btn_open_browser.Location = new System.Drawing.Point(12, 12);
+            this.btn_open_browser.Name = "btn_open_browser";
+            this.btn_open_browser.Size = new System.Drawing.Size(270, 80);
+            this.btn_open_browser.TabIndex = 0;
+            this.btn_open_browser.Text = "Open Chrome";
+            this.btn_open_browser.UseVisualStyleBackColor = true;
+            this.btn_open_browser.Click += new System.EventHandler(this.btn_open_browser_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 691);
+            this.Controls.Add(this.btn_open_browser);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_open_browser;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace ContestEnterer
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btn_open_browser_Click(object sender, EventArgs e)
+        {
+            IWebDriver webDriver;
+            webDriver = new ChromeDriver(); // TODO: put default, updateable in config
+            webDriver.Url = "http://www.google.com";
+        }
+    }
+}
